0c2189496f2f2bef06af3a9d7e08a81bac5baeec
subhamstark <subhamofficial98@gmail.com>
2020-09-22 19:53:45 +0530

    UC6 Done

--- src/EmpWages.java
+++ src/EmpWages.java
@@ -4,22 +4,34 @@ public class EmpWages {
 	final static int wagePerHour = 20;
 	final static int TotalWorkingHourFullTime=8;
 	final static int TotalWorkHourPartTime=8;
-	final static int TotalWorkDayPerMonth=20;
+	final static int maxWorkDayPerMonth=20;
+	final static int maxWorkHour=100;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int randomMark =(int)(Math.random()*10%3);
+		int TotalWorkHour=0;
+		int TotalWorkDays=0;
 		
-		switch(randomMark) {
-		case 1:
-			System.out.println("Total Monthly wage Full Time : "+ TotalWorkDayPerMonth*TotalWorkingHourFullTime*wagePerHour);
-			break;
-		case 2:
-			System.out.println("Total Monthly wage Full Time : "+ TotalWorkDayPerMonth*TotalWorkingHourFullTime*wagePerHour);
-			break;
-		default:
-			System.out.println("N/A Not an Employee");
+		while(TotalWorkHour<=maxWorkHour && TotalWorkDays<=maxWorkDayPerMonth) {
+			int randomMark =(int)(Math.random()*10%3);
+			if(randomMark!=0)
+			{
+			switch(randomMark) {
+				case 1:
+					TotalWorkHour+=TotalWorkingHourFullTime;
+					break;
+				case 2:
+					TotalWorkHour+=TotalWorkHourPartTime;
+					break;
+				default:
+					break;
+				}
+			}
+			TotalWorkDays++;
+		
 		}
+		
+		System.out.println("Total Work days : " + TotalWorkDays + " Total Work Hour " + TotalWorkHour + "Total Monthly Wage : " + TotalWorkHour*TotalWorkDays*wagePerHour);
 	}
 
 }
