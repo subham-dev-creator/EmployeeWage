7d3623a8784fe3ca91f31ad138aa91a1ec94ba03
subhamstark <subhamofficial98@gmail.com>
2020-09-22 17:16:46 +0530

    Setup

---
+++ src/EmpWages.java
@@ -0,0 +1,12 @@
+
+public class EmpWages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee  \r\n" + 
+				"Wage Computation  \r\n" + 
+				"Program\r\n" + 
+				"");
+	}
+
+}
