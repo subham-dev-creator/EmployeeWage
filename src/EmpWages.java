3100736ca64d1bb57716377a80d3846cb535a981
subhamstark <subhamofficial98@gmail.com>
2020-09-22 17:56:22 +0530

    UC2 Done

--- src/EmpWages.java
+++ src/EmpWages.java
@@ -1,12 +1,24 @@
 
 public class EmpWages {
-
+	
+	final static int wagePerHour = 20;
+	final static int TotalWorkingHour=8;
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee  \r\n" + 
-				"Wage Computation  \r\n" + 
-				"Program\r\n" + 
-				"");
+		int randomMark =(int)(Math.random()*10%2);
+		if(randomMark==1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
+		
+		if(randomMark==1) {
+			System.out.println("Total wage of Employee "+ TotalWorkingHour*wagePerHour);
+		}
+		else {
+			System.out.println("N/A as Employee is Absent");
+		}
 	}
 
 }
