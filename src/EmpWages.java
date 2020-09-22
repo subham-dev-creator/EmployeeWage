c590b06e5749d1c7ed9f426f8897fe5284c81edd
subhamstark <subhamofficial98@gmail.com>
2020-09-22 18:58:22 +0530

    UC5 Done

--- src/EmpWages.java
+++ src/EmpWages.java
@@ -4,7 +4,7 @@ public class EmpWages {
 	final static int wagePerHour = 20;
 	final static int TotalWorkingHourFullTime=8;
 	final static int TotalWorkHourPartTime=8;
-	
+	final static int TotalWorkDayPerMonth=20;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,10 +12,10 @@ public class EmpWages {
 		
 		switch(randomMark) {
 		case 1:
-			System.out.println("Total wage Full Time : "+ TotalWorkingHourFullTime*wagePerHour);
+			System.out.println("Total Monthly wage Full Time : "+ TotalWorkDayPerMonth*TotalWorkingHourFullTime*wagePerHour);
 			break;
 		case 2:
-			System.out.println("Total wage Full Time : "+ TotalWorkingHourFullTime*wagePerHour);
+			System.out.println("Total Monthly wage Full Time : "+ TotalWorkDayPerMonth*TotalWorkingHourFullTime*wagePerHour);
 			break;
 		default:
 			System.out.println("N/A Not an Employee");
