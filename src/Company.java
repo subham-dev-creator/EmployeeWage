dd626159828af0dd5d18dbc190af6aad90dca504
subhamstark <subhamofficial98@gmail.com>
2020-09-24 23:36:05 +0530

    UC8 done

---
+++ src/Company.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Company extends EmpWages {
+	
+	//Variables
+	int CompanyId;
+	static int wagePerHour;
+	static int TotalWorkingHourFullTime;
+	static int TotalWorkHourPartTime;
+	static int maxWorkDayPerMonth;
+	static int maxWorkHour;
+	int Id=1;
+	Vector<EmpWages> emp = new Vector<EmpWages>() ;
+	
+	//Constructor
+	Company(){
+		this.CompanyId=Id++;
+		wagePerHour = 20;
+		TotalWorkingHourFullTime=8;
+		TotalWorkHourPartTime=8;
+		maxWorkDayPerMonth=20;
+		maxWorkHour=100;
+	}
+	
+	static //Functions
+	int[] calculateWage(int TotalWorkHour,int TotalWorkDays) {
+		int[] WageHourDaysArray=new int[3];
+		int CurWage=0;
+		while(TotalWorkHour<=maxWorkHour && TotalWorkDays<=maxWorkDayPerMonth) {
+			int randomMark =(int)(Math.random()*10%3);
+			if(randomMark!=0)
+			{
+			switch(randomMark) {
+				case 1:
+					//Full time
+					TotalWorkHour+=TotalWorkingHourFullTime;
+					break;
+				case 2:
+					//Part Time
+					TotalWorkHour+=TotalWorkHourPartTime;
+					break;
+				default:
+					//No Work
+					break;
+				}
+			}
+			TotalWorkDays++;
+		}
+		CurWage=TotalWorkHour*TotalWorkDays*wagePerHour;
+		WageHourDaysArray[0]=CurWage;
+		WageHourDaysArray[1]=TotalWorkHour;
+		WageHourDaysArray[2]=TotalWorkDays;
+		return WageHourDaysArray;
+	}
+	
+	public static void main(String[] args) {
+		int NumOfCompany ;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter Count of Companies ");
+		NumOfCompany = sc.nextInt();
+		int[] NumOfEmp=new int[NumOfCompany];
+		Company[] c =new Company[NumOfCompany];
+		
+		for(int i=0;i<NumOfCompany;i++) {
+			System.out.println("Enter Count of Employees ");
+			NumOfEmp[i]=sc.nextInt();
+			
+			c[i] = new Company();
+			for(int j=0;j<NumOfEmp[i];j++) {
+				EmpWages e=new EmpWages();
+				int[] WageHourDaysArray=new int[3];
+				
+				WageHourDaysArray=calculateWage(e.TotalWorkDays,e.TotalWorkHour);
+				e.MonthlyWage=WageHourDaysArray[0];
+				e.TotalWorkDays=WageHourDaysArray[1];
+				e.TotalWorkHour=WageHourDaysArray[2];
+				c[i].emp.add(e);
+				
+				System.out.println("Company : " + i + " Employee " + j + " Has MonthlyWage " +c[i].emp.elementAt(j).MonthlyWage);
+			
+			}
+		
+		}
+		sc.close();
+	}
+}
--- src/EmpWages.java
+++ src/EmpWages.java
@@ -8,46 +8,16 @@ public class EmpWages {
 		TotalWorkHour=0;
 		TotalWorkDays=0;
 	}
-	final static int wagePerHour = 20;
-	final static int TotalWorkingHourFullTime=8;
-	final static int TotalWorkHourPartTime=8;
-	final static int maxWorkDayPerMonth=20;
-	final static int maxWorkHour=100;
-	
-	void calculateWage() {
-		int CurWage=0;
-		while(TotalWorkHour<=maxWorkHour && TotalWorkDays<=maxWorkDayPerMonth) {
-			int randomMark =(int)(Math.random()*10%3);
-			if(randomMark!=0)
-			{
-			switch(randomMark) {
-				case 1:
-					//Full time
-					TotalWorkHour+=TotalWorkingHourFullTime;
-					break;
-				case 2:
-					//Part Time
-					TotalWorkHour+=TotalWorkHourPartTime;
-					break;
-				default:
-					//No Work
-					break;
-				}
-			}
-			TotalWorkDays++;
-		}
-		CurWage=TotalWorkHour*TotalWorkDays*wagePerHour;
-		this.MonthlyWage= CurWage;
-	}
-
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		EmpWages emp=new EmpWages();
-		emp.calculateWage();
+		EmpWages e=new EmpWages();
 		
 		
 		System.out.println("Total Work days : " + emp.TotalWorkDays + " Total Work Hour " + emp.TotalWorkHour + " Total Monthly Wage : " + emp.MonthlyWage);
+
+		System.out.println("Total Work days : " + e.TotalWorkDays + " Total Work Hour " + e.TotalWorkHour + " Total Monthly Wage : " + e.MonthlyWage);
 	}
 
 }
